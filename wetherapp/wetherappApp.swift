0bb1cd6bcba4db84903cdf40fc3eeef7b88b3285
mrreiaz <reiazbubt@gmail.com>
2024-05-26 14:45:20 +0900

    Initial Commit

---
+++ wetherapp/wetherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  wetherappApp.swift
+//  wetherapp
+//
+//  Created by Abdul Halim Reiaz on 2024/05/26.
+//
+
+import SwiftUI
+
+@main
+struct wetherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
